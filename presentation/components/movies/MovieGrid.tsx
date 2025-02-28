172600669c1ab3b2a834ad33a1e6f8fcebbeee69
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-02-28 12:00:40 -0600

    Componente MovieGrid, añadido

---
+++ presentation/components/movies/MovieGrid.tsx
@@ -0,0 +1,28 @@
+import { Movie } from '@/infrastructure/interfaces/movie.interface'
+import { View, Text, FlatList } from 'react-native'
+import MoviePoster from './MoviePoster'
+
+interface Props {
+  movies?: Movie[]
+  className?: string
+}
+const MovieGrid = ({ movies=[], className='' }: Props) => {
+  return (
+    <FlatList
+      data={movies}
+      keyExtractor={(item) => item.id.toString()}
+      numColumns={3}
+      renderItem={({ item }) => (
+        <MoviePoster id={item.id} title={item.title} poster={item.poster} />
+      )}
+      columnWrapperStyle={{ justifyContent: 'space-between', paddingHorizontal: 8 }}
+      contentContainerStyle={{ gap: 10, paddingBottom: 20 }}
+
+      initialNumToRender={9}
+      maxToRenderPerBatch={9}
+      windowSize={5}
+    />
+  )
+}
+
+export default MovieGrid
